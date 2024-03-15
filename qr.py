f11aa6fa22156fed42818bcf1c13f5d67a62690b
John Anthony Rico <johnrico364@gmail.com>
2024-03-15 19:23:38 +0800

    Fix: Qrcode enlarge fix

--- qr.py
+++ qr.py
@@ -1,6 +1,14 @@
 import segno
 
-qrcode = segno.make_qr('https://chat.openai.com/')
-apex = segno.make_qr('https://apex.oracle.com/pls/apex/f?p=4550:1:14040423686837:::::')
-qrcode.save("GPT.png")
-apex.save("apex.png")
+# qrcode = segno.make_qr('https://chat.openai.com/')
+# apex = segno.make_qr('https://apex.oracle.com/pls/apex/f?p=4550:1:14040423686837:::::')
+# qrcode.save("GPT.png")
+# apex.save("apex.png")
+
+print("Link to QR Code Generator");
+
+link =  input("Put your link here: ");
+_qrCode = segno.make_qr(link);
+_qrCode.save("QR Code.png", scale=15);
+
+print("QR Code generated....");
